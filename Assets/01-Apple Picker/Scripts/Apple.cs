1c345d2dff91440a37698a66b4f72f9620448a2e
kilykd <123680938+kilykd@users.noreply.github.com>
2023-02-02 12:59:53 -0500

    Destroying Apples
    Changed Apple script to destroy apple object once they reach a y position of -20.

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Apple : MonoBehaviour{
+	
+	public static float bottomY = -20f;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+		
+		if (transform.position.y < bottomY){
+		Destroy(this.gameObject);
     }
+  }
 }
