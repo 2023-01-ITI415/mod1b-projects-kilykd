204afc1334bbb510aad735c13d2c3e049c2429db
kilykd <123680938+kilykd@users.noreply.github.com>
2023-02-02 12:24:29 -0500

    AppleTree Scripting
    Starting making changes in the AppleTree script to make the apple tree move back and forth on the screen. Set edge boundaries so the tree will move change direction once it hits a left or right edge.

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+      [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change direction
+    public float chanceToChangeDirection;
+
+    // Rate at which Apples will be dropped
+    public float secondsBetweenAppleDrop;
+
+    void Start () {
+        // Dropping apples every second
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+		
+        // Basic Movement
+		Vector3 pos = transform.position;      
+        pos.x += speed * Time.deltaTime;       
+        transform.position = pos; 
+		
+        // Changing Direction
+		if ( pos.x < -leftAndRightEdge ) {      
+           speed = Mathf.Abs(speed); // Move right
+       } else if ( pos.x > leftAndRightEdge ) {
+           speed = -Mathf.Abs(speed); // Move left
+       }
     }
 }
