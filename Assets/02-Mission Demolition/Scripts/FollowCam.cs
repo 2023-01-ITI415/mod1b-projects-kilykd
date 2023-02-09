d933bcb5cb3beca43e5798c21cb205f6331a1f45
kilykd <123680938+kilykd@users.noreply.github.com>
2023-02-09 15:22:15 -0500

    Follow Cam
    Added  a followcam script to follow the projectile as it it launched. Also made the script set the camera back to the slingshot after the projectile stops moving so you can take another shot.

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
-     static public GameObject POI; // The static point of interest             
+     static public GameObject POI; // The static point of interest     
+
+	 [Header("Inscribed")]
+	 public float easing = 0.05f;
+	 public Vector2 minXY = Vector2.zero; //Vector2.zero is [0,0]
  
      [Header("Dynamic")]
      public float camZ; // The desired Z pos of the camera 
@@ -14,11 +18,25 @@ public class FollowCam : MonoBehaviour
      }
  
      void FixedUpdate () {
-         // A single-line if statement doesn’t require braces
-         if (POI == null) return; // if there is no POI, then return           
+         
+		Vector3 destination = Vector3.zero;                                  
  
-         // Get the position of the poi
-         Vector3 destination = POI.transform.position;
+        if ( POI != null ) {                                                  
+             // If the POI has a Rigidbody, check to see if it is sleeping
+             Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+             if ( ( poiRigid != null ) && poiRigid.IsSleeping() ) {           
+                 POI = null;
+             }
+        }
+ 
+        if ( POI != null ) {                                                  
+             destination = POI.transform.position;
+        }
+		 //Limit the minimum values of destination.x & destination.y
+		 destination.x = Mathf.Max( minXY.x, destination.x );
+         destination.y = Mathf.Max( minXY.y, destination.y );
+		 //Interpolate from the current Camera position toward destination
+		 destination = Vector3.Lerp(transform.position, destination, easing);
          // Force destination.z to be camZ to keep the camera far enough away
          destination.z = camZ;
          // Set the camera to the destination
