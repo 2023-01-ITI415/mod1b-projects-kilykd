3934c68837b35db0572dc107375d9676bcea6913
kilykd <123680938+kilykd@users.noreply.github.com>
2023-02-16 16:03:00 -0500

    Difficulty increase
    First attempt at making the difficulty increase after a certain number of bumpers have been instantiated

--- Assets/03-Prototype1/Scripts/BumperMachine.cs
+++ Assets/03-Prototype1/Scripts/BumperMachine.cs
@@ -8,28 +8,35 @@ public class BumperMachine : MonoBehaviour
 	//Prefab for instantiating bumpers
 	public GameObject bumperPrefab;
 	
+	private int bumpersLaunched = 0;
 	
 	//Seconds between bumper instantiations 
 	public float bumperLaunchDelay = 1f;
 	
-	
+	public Bumper myBumper;
     
     void Start()
 	{
         //Start launching bumpers
 		Invoke("LaunchBumper", 2f);
+		
     }
 	
 	void LaunchBumper()
 	{
 		GameObject bumper = Instantiate<GameObject>(bumperPrefab);
 		bumper.transform.position = transform.position;
+		bumpersLaunched++;
+
+		if(bumpersLaunched == 9)
+		{
+			bumperLaunchDelay -= .5f;
+			myBumper.speed *= 1.5f;
+		}
+		
 		Invoke("LaunchBumper", bumperLaunchDelay);
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   
 }
