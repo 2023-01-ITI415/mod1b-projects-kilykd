ac2d5bdfed8140a54544403ab5d08cde5db5a486
kilykd <123680938+kilykd@users.noreply.github.com>
2023-02-15 21:20:40 -0500

    Bumper Deletion
    Bumper prefabs are now destroyed once they are out of the camera and hit a specific x-value

--- Assets/03-Prototype1/Scripts/Bumper.cs
+++ Assets/03-Prototype1/Scripts/Bumper.cs
@@ -6,14 +6,20 @@ public class Bumper : MonoBehaviour
 {
 	//Speed at which bumpers travel
     public float speed = 1f;
+	public static float leftMostY = -36f;
 	
     // Update is called once per frame
     void Update()
     {
-        //Basic Movement
+        //Moves bumpers across the screen from righ to left
 		Vector3 pos = transform.position;
 		pos.x += -1 * speed * Time.deltaTime;
 		transform.position = pos;
+		
+		if (transform.position.x < leftMostY) 
+		{
+			Destroy(this.gameObject);
+		}
 		
     }
 }
